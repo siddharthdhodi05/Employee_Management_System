903fba5607fc82f732c21d6d3c559f40b02fdfe3
siddharthdhodi05 <siddharth.dhodi.work@gmail.com>
2024-12-12 21:33:45 +0530

    Project Complete

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+   content: [
+    './index.html', // Add this line to include your HTML files
+    './src/**/*.{js,ts}', // This will include JS/TS files in the src folder (if any)
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
